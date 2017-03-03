96aefb606cb4bf7ed23cf02a8889245c96c69261
simontollstern <simoon909@hotmail.com>
2017-03-03 14:37:51 +0100

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :posts, :except => :show
+  root 'posts#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+end
